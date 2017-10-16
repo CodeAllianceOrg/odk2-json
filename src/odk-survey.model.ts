b75fc941892ec4e0b6a66ec07b0709c0e80c8eb4
Tristan Kernan <russon77@gmail.com>
2017-10-16 13:51:02 -0400

    feat(core): add support for sections

--- src/odk-survey.model.ts
+++ src/odk-survey.model.ts
@@ -3,12 +3,44 @@ import * as XLSX from 'xlsx';
 interface ISetting {
     readonly setting_name: string;
     readonly value?: string;
+    readonly display?: string;
     readonly 'display.title'?: string;
 }
 
+export interface ISection {
+    readonly section_name: string;
+}
+
+export interface ISurveyRow {
+    readonly clause: string;
+    readonly 'display.text': string;
+    readonly name: string;
+    readonly type: string;
+}
+
+const BASE_SURVEY_ROW: ISurveyRow = {
+    clause: '',
+    'display.text': '',
+    name: '',
+    type: ''
+};
+
+function createSurveyRow(partial?: Partial<ISurveyRow>): ISurveyRow {
+    return {
+        ...BASE_SURVEY_ROW,
+        ...partial
+    };
+}
+
 export class ODKSurvey {
-    public static fromJSON(input: any): ODKSurvey {
-        return new ODKSurvey();
+    private sections: ISection[] = [];
+
+    public static fromJSON(input: ISection[]): ODKSurvey {
+        const survey = new ODKSurvey();
+
+        survey.sections = input;
+
+        return survey;
     }
 
     public toXLSXBase64(): string {
@@ -29,13 +61,38 @@ export class ODKSurvey {
             }
         ];
 
-        const data = [
-            {
-                'display.text': 'display.text',
-                name: 'name',
-                type: 'text'
-            }
-        ];
+        const data: ISurveyRow[] = [];
+
+        this.sections.forEach(section => {
+            // append a sheet for each section
+
+            XLSX.utils.book_append_sheet(
+                wb,
+                XLSX.utils.json_to_sheet([
+                    createSurveyRow({
+                        type: 'text',
+                        name: 'name',
+                        'display.text': 'Enter name:'
+                    })
+                ]),
+                section.section_name
+            );
+
+            // add the section to the main survey sheet
+
+            data.push(
+                createSurveyRow({
+                    clause: `do section ${section.section_name}`
+                })
+            );
+
+            // add the display data to the settings sheet
+
+            settings.push({
+                setting_name: section.section_name,
+                display: ''
+            });
+        });
 
         XLSX.utils.book_append_sheet(
             wb,
--- test/odk-format-converter.test.ts
+++ test/odk-format-converter.test.ts
@@ -1,10 +1,18 @@
-import { ODKSurvey } from '../src/odk-survey.model';
+debugger;
+
+import { ODKSurvey, ISection, ISurveyRow } from '../src/odk-survey.model';
 import { get_header_row, to_json } from './utils';
 import * as XLSXConverter2 from 'jswebviewer/xlsxconverter/XLSXConverter2';
 import * as XLSX from 'xlsx';
 
 const ODK2_MIN_NUM_COLS = 3;
 const ODK2_REQUIRED_COLS = ['type', 'name', 'display.text'];
+
+const EXAMPLE_SURVEY = [
+    {
+        section_name: 'helloworld'
+    }
+];
 
 describe('ODKSurvey', () => {
     it('exports XLSX with the correct columns', () => {
@@ -18,7 +26,7 @@ describe('ODKSurvey', () => {
           The best approach is to validate the desired worksheet and ensure it has the expected number of rows or columns. Extracting the range is extremely simple
         */
 
-        const subject = ODKSurvey.fromJSON({});
+        const subject = ODKSurvey.fromJSON(EXAMPLE_SURVEY);
 
         const xlsx = subject.toXLSXBase64();
 
@@ -26,20 +34,13 @@ describe('ODKSurvey', () => {
 
         expect(() => (wb = XLSX.read(xlsx, { type: 'base64' }))).not.toThrow();
 
-        const range = XLSX.utils.decode_range(
-            wb.Sheets[wb.SheetNames[0]]['!ref']
-        );
-        const ncols = range.e.c - range.s.c + 1;
-
-        expect(ncols).toBeGreaterThanOrEqual(ODK2_MIN_NUM_COLS);
-
         const colNames = get_header_row(wb.Sheets.survey);
 
         ODK2_REQUIRED_COLS.forEach(col => expect(colNames).toContain(col));
     });
 
     it('exports in valid ODK 2.0 XLSX', () => {
-        const subject = ODKSurvey.fromJSON({});
+        const subject = ODKSurvey.fromJSON(EXAMPLE_SURVEY);
 
         const xlsx = subject.toXLSXBase64();
 
@@ -48,5 +49,62 @@ describe('ODKSurvey', () => {
         const jsonWorkbook = to_json(wb);
 
         expect(() => XLSXConverter2.processJSONWb(jsonWorkbook)).not.toThrow();
+    });
+
+    describe('sections', () => {
+        it('creates an excel worksheet for each section', () => {
+            const sections: ISection[] = [
+                {
+                    section_name: 'testsection'
+                },
+                {
+                    section_name: 'anothersection'
+                }
+            ];
+
+            const subject = ODKSurvey.fromJSON(sections);
+
+            const xlsx = subject.toXLSXBase64();
+
+            const wb = XLSX.read(xlsx, { type: 'base64' });
+
+            sections.forEach(section => {
+                expect(wb.SheetNames).toContain(section.section_name);
+            });
+        });
+
+        it('adds each section to the main survey sheet', () => {
+            const sections: ISection[] = [
+                {
+                    section_name: 'testsection'
+                },
+                {
+                    section_name: 'anothersection'
+                }
+            ];
+
+            const subject = ODKSurvey.fromJSON(sections);
+
+            const xlsx = subject.toXLSXBase64();
+
+            const wb = XLSX.read(xlsx, { type: 'base64' });
+
+            // get the clause column data in order
+            let clauses: string[] = XLSX.utils
+                .sheet_to_json<ISurveyRow>(wb.Sheets.survey)
+                .map(val => val.clause);
+
+            // limit to `do section` values
+            clauses = clauses.filter(
+                clause => clause.indexOf('do section') !== -1
+            );
+
+            // remove 'do section'
+            clauses = clauses.map(clause => clause.replace('do section ', ''));
+
+            expect(sections.map(section => section.section_name)).toEqual(
+                clauses
+            );
+        });
     });
 });
