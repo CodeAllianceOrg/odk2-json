cfc44c11483ffd2afd948abf6b1c87fa5f50ad9d
Tristan K <russon77@gmail.com>
2017-10-16 14:59:39 -0400

    feat(build): add build files for distribution

---
+++ dist/types/odk-survey.model.d.ts
@@ -0,0 +1,20 @@
+export interface IQuestion {
+    readonly type: 'text';
+    readonly name: string;
+    readonly 'display.text': string;
+}
+export interface ISection {
+    readonly section_name: string;
+    readonly questions: IQuestion[];
+}
+export interface ISurveyRow {
+    readonly clause: string;
+    readonly 'display.text': string;
+    readonly name: string;
+    readonly type: string;
+}
+export declare class ODKSurvey {
+    private sections;
+    static fromJSON(input: ISection[]): ODKSurvey;
+    toXLSXBase64(): string;
+}
